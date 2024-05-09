6345f6822489260253ce6927a8b115bbb47e3b5d
ylei <2457738122@qq.com>
2024-05-09 10:36:13 +0800

    优化压缩字典树

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -1,20 +1,31 @@
 import sys
 
 sys.path.append("../")
+import random
+
 from flyflower.trie import ZipTrie
+
+
+def gen_paragraph():
+    while True:
+        # 随机长度
+        n = random.randint(1, 10)
+        # 随机字符串
+        yield "".join(random.choices("ab", k=n))
 
 
 def main():
     # 测试空初始化
     trie = ZipTrie()
-    paragraphs = ["abdabd", "adad", "dad", "adadeeeee", "ad", "dad"]
-    for paragraph in paragraphs:
-        trie.insert(paragraph)
-    for paragraph in paragraphs:
-        print(trie.exist(paragraph))
-    error_paragraphs = ["xxxx", "dax", "ada", "adade", "adadeeee"]
-    for paragraph in error_paragraphs:
-        print(trie.exist(paragraph))
+    batch = 1000000000
+    iter = gen_paragraph()
+    while batch > 0:
+        # 随机长度
+        paragraph = next(iter)
+        trie.add(paragraph)
+        ok = trie.exist(paragraph)
+        assert ok
+        batch -= 1
 
 
 if __name__ == "__main__":
