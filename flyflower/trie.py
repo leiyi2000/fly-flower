f243830693fb8c74dbbc1e62213c2c732f1890f2
ylei <2457738122@qq.com>
2024-05-09 10:36:13 +0800

    优化压缩字典树

--- flyflower/trie.py
+++ flyflower/trie.py
@@ -1,121 +1,112 @@
 from typing import Dict, Tuple
 
 
-class TrieNode:
-    """节点"""
-
-    def __init__(
-        self,
-        value: str = "",
-        children: Dict[str, "TrieNode"] | None = None,
-        end: bool = False,
-    ):
-        self.children: Dict[str, TrieNode] | None = children
-        self.value = value
-        self.end = end
-
-    def add(self, node: "TrieNode"):
-        if self.children is None:
-            self.children = {}
-        self.children[node.value] = node
-
-
 class ZipTrie:
     """压缩字典树"""
 
-    def __init__(self):
-        self.root = TrieNode()
+    def __init__(
+        self,
+        children: Dict[str, "ZipTrie"] | None = None,
+        end: bool = False,
+    ):
+        self.children: Dict[str, ZipTrie] | None = children
+        self.end = end
 
-    def _find(self, paragraph: str) -> Tuple[TrieNode, int]:
+    def add(self, paragraph: str):
+        """添加子节点.
+
+        Args:
+            paragraph (str): 节点.
+        """
+        node, match_length = self.find(paragraph)
+        if match_length == len(paragraph):
+            node.end = True
+            return
+        # 剩余添加段落
+        paragraph = paragraph[match_length:]
+        if node.children is None:
+            node.children = {}
+        prefix_length = 0
+        for key in node.children:
+            # 查找公共前缀
+            while prefix_length < len(paragraph) and prefix_length < len(key):
+                if paragraph[prefix_length] == key[prefix_length]:
+                    prefix_length += 1
+                else:
+                    break
+            if prefix_length > 0:
+                # 公共前缀长度大于0，则分裂
+                prefix_node = node.fission(key, prefix_length)
+                # 处理剩余添加段落
+                if prefix_length < len(paragraph):
+                    prefix_node.children[paragraph[prefix_length:]] = ZipTrie(end=True)
+                else:
+                    # 公共前缀长度等于paragraph, 直接标记end
+                    node.children[paragraph].end = True
+                break
+        # 没有公共前缀
+        if prefix_length == 0:
+            node.children[paragraph] = ZipTrie(end=True)
+
+    def remove(self, paragraph: str):
+        """移除子节点.
+
+        Args:
+            node (paragraph): 节点.
+        """
+        # TODO
+        pass
+
+    def fission(self, paragraph: str, length: int) -> "ZipTrie":
+        """节点裂解.
+
+        Args:
+            paragraph (str): 段落.
+            length (int): 分裂段落长度.
+
+        Returns:
+            ZipTrie: 分裂后的前缀节点.
+        """
+        if len(paragraph) == length:
+            self.children[paragraph].end = True
+            prefix_node = self.children[paragraph]
+        else:
+            suffix_node = self.children.pop(paragraph)
+            prefix = paragraph[:length]
+            suffix = paragraph[length:]
+            prefix_node = self.children[prefix] = ZipTrie(
+                children={suffix: suffix_node}, end=False
+            )
+        return prefix_node
+
+    def find(self, paragraph: str) -> Tuple["ZipTrie", int]:
         """查询段落是否存在.
 
         Args:
             paragraph (str): 段落.
 
         Returns:
-            Tuple[TrieNode, int]: 节点和匹配长度.
+            Tuple[ZipTrie, int]: 节点和匹配长度.
         """
         prefix = ""
-        node = self.root
-        match_count = 0
+        node = self
+        match_length = 0
         for char in paragraph:
             prefix += char
             if node.children and prefix in node.children:
                 node = node.children[prefix]
-                match_count += len(prefix)
+                match_length += len(prefix)
                 prefix = ""
-        return node, match_count
+        return node, match_length
 
-    def _pop_from_children(
-        self, paragraph: str, node: TrieNode
-    ) -> Tuple[TrieNode | None, int]:
-        """查询当前节点的所有子节点的公共前缀.
+    def exist(self, paragraph: str) -> bool:
+        """判断段落是否存在.
 
         Args:
             paragraph (str): 段落.
-            node (TrieNode): 节点.
 
         Returns:
-            Tuple[TrieNode | None, int]: 与paragraph有相同前缀的节点, 最后一次相同字符的索引.
+            bool: True 存在 or False 不存在.
         """
-        if node.children is None:
-            return None, -1
-        for key in node.children:
-            i = 0
-            while i < len(paragraph) and i < len(key):
-                if paragraph[i] != key[i]:
-                    break
-                i += 1
-            i -= 1
-            if i >= 0:
-                return node.children.pop(key), i
-        return None, -1
-
-    def insert(self, paragraph: str) -> None:
-        """
-        Inserts a paragraph into the trie.
-        """
-        # 找出和当前层节点匹配的子节点
-        node, match_count = self._find(paragraph)
-        if match_count == len(paragraph):
-            node.end = True
-            return
-        # 将剩余不匹配的字符添加到子节点中
-        suffix = paragraph[match_count:]
-        children, prefix_index = self._pop_from_children(suffix, node)
-        if prefix_index == -1:
-            # 新建节点
-            node.add(TrieNode(value=suffix, end=True))
-        else:
-            # 记录当前孩子节点数据
-            grand_children = children.children
-            children_value = children.value
-            children_end = children.end
-            # 重置孩子节点数据
-            children.children = None
-            children.value = suffix[: prefix_index + 1]
-            if prefix_index + 1 < len(children_value):
-                children.add(
-                    TrieNode(
-                        value=children_value[prefix_index + 1 :],
-                        children=grand_children,
-                        end=children_end,
-                    )
-                )
-            elif prefix_index == len(children_value) - 1:
-                children.end = children_end
-                children.children = grand_children
-            if prefix_index + 1 < len(suffix):
-                children.add(TrieNode(value=suffix[prefix_index + 1 :], end=True))
-            elif prefix_index == len(suffix) - 1:
-                children.end = True
-            # 添加到父节点
-            node.add(children)
-        return
-
-    def exist(self, paragraph: str) -> bool:
-        """
-        Returns if the paragraph prefixes are similar.
-        """
-        node, match_count = self._find(paragraph)
-        return node.end and match_count == len(paragraph)
+        node, match_length = self.find(paragraph)
+        return node.end and match_length == len(paragraph)
